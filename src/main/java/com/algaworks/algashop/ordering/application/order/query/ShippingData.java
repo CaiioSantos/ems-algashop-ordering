913e4504262d5cefd81d2438c0953bec0597e008
CAIO <caiohgs13@gmail.com>
2026-03-17 23:05:25 -0300

    criando teste e contrato de erro quando o produto é buscado pelo ID e nao é encontrado

--- src/main/java/com/algaworks/algashop/ordering/application/order/query/ShippingData.java
+++ src/main/java/com/algaworks/algashop/ordering/application/order/query/ShippingData.java
@@ -7,6 +7,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Data
@@ -16,8 +17,8 @@ import java.time.LocalDateTime;
 public class ShippingData {
 
     private BigDecimal cost;
-    private LocalDateTime expectedDate;
-    private RecipientData recipientData;
-    private AddressData addressData;
+    private LocalDate expectedDate;
+    private RecipientData recipient;
+    private AddressData address;
 
 }
