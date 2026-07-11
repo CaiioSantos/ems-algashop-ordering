55d61c686643c7e7aa75b9dd428659a06c28f5ba
CAIO <caiohgs13@gmail.com>
2026-07-10 21:28:12 -0300

    adicionando validações e tratamento de erros na API de cálculo de custo de entrega, ajustando respostas para requisições inválidas e configurando timeouts para chamadas externas

--- src/main/java/com/algaworks/algashop/ordering/core/ports/in/commons/AddressData.java
+++ src/main/java/com/algaworks/algashop/ordering/core/ports/in/commons/AddressData.java
@@ -1,6 +1,7 @@
 package com.algaworks.algashop.ordering.core.ports.in.commons;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -30,5 +31,6 @@ public class AddressData {
     String state;
 
     @NotBlank
+    @Size(min = 5, max = 10)
     String zipCode;
 }
